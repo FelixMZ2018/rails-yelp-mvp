d69041edce50c21f0dbb29b837382b9f5e6858f0
Felix Menzel <FelixinLondon@googlemail.com>
2020-08-12 14:51:42 +0200

    working out the last issues in the review system

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+10.times do
+    rest = Restaurant.new(name: Faker::Restaurant.name ,address: Faker::Address.street_address, category: category.sample, phone_number: Faker::PhoneNumber.phone_number_with_country_code )
+    p rest
+    rest.save!
+    10.times do 
+        review = Review.new(title: Faker::Marketing.buzzwords,content: Faker::Restaurant.review, rating: rand(0...5))
+        review.restaurant = rest
+        p review
+        review.save!
+    end
+end
